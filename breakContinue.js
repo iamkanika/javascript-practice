f399b1784ebf43c023c3b0fb4fda504147d24bc7
Kanika <kanikagarg2707@gmail.com>
2024-07-13 13:39:04 +0530

    Day3

---
+++ breakContinue.js
@@ -0,0 +1,17 @@
+let i=0;
+while(i<=9){
+    if(i==4){
+        break;
+    }
+    console.log(i);
+    i++;   
+}
+console.log("Example of continue..");
+let a = 0;
+while(i<=9){
+    if(i==4){
+        continue;
+    }
+    console.log(i);
+    i++;
+}
