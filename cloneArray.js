84cdf5c5910456a20b62cb63794952c397356340
Kanika <kanikagarg2707@gmail.com>
2024-07-14 20:52:59 +0530

    Day4

--- cloneArray.js
+++ cloneArray.js
@@ -1,14 +1,22 @@
 let array1 = ["item1","item2","item3"];
 let array2 = array1.slice(0);
 let array3 = [].concat(array1);
+//spread operator..
 let array4 = [...array1];
 console.log(array1);
 console.log(array2);
 console.log(array3);
 console.log(array4);
+array2 = [...array1, "item4" , "item5"];
+console.log(array2);
 array1.push("item4");
 console.log("After Updating");
 console.log(array1);
 console.log(array2);
 console.log(array3);
 console.log(array4);
+console.log(array1===array2);
+array2 = [...array1, "item4" , "item5"];
+console.log(array2);
+let array5 = [...array1, ...array2];
+console.log(array5);
---
+++ forLoopArray.js
@@ -0,0 +1,27 @@
+//for loop in array
+let fruits=["apple","banana","pineapple"];
+for(let i=0; i<fruits.length; i++){
+    console.log(fruits[i]);
+}
+//print in uppercase
+
+for(let a=0; a<fruits.length; a++){
+    console.log(fruits[a].toUpperCase());
+}
+let fruits2=[];
+for(let a=0; a<fruits.length; a++){
+    fruits2.push(fruits[a].toUpperCase());
+}
+console.log(fruits2);
+//use of constant in arrays
+const frt=["apple","mango"];
+//we can not frt=["pinapple "] in case of constant
+frt.push("grapes");
+console.log(frt);
+// but in case of let
+let frt2 =["Aaruhi"];
+console.log(frt2);
+frt2 = ["litchi"];
+console.log(frt2);
+frt2.push("banana");
+console.log(frt2);
