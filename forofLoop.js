3fa05aa8224ba3dd5171c0ee9bfd871c4f80ac75
Kanika <kanikagarg2707@gmail.com>
2024-07-15 23:24:58 +0530

    Day5

---
+++ forofLoop.js
@@ -0,0 +1,5 @@
+//for of loop in array
+let fruits = ["banana","apple","grapes"]
+for(let fruit of fruits){
+console.log(fruit);
+}
---
+++ whileArray.js
@@ -0,0 +1,6 @@
+const array = [1,2,3];
+let i = 0;
+while(i< array.length){
+    console.log(array[i]);
+    i++;
+}
